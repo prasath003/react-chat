690fac5b1daa4bcef475f073402ef60dc0cccbdb
prasath <prasath.muthaiyan@gmail.com>
2020-12-22 11:31:15 +0530

    feat: project configuration

---
+++ src/components/List/List.lazy.tsx
@@ -0,0 +1,13 @@
+import React, { lazy, Suspense } from 'react';
+
+const LazyList = lazy(() => import('./list'));
+
+const List = (
+  props: JSX.IntrinsicAttributes & { children?: React.ReactNode }
+) => (
+  <Suspense fallback={null}>
+    <LazyList {...props} />
+  </Suspense>
+);
+
+export default List;
---
+++ src/components/List/List.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './List.module.css';
+
+const List: React.FC = () => (
+  <div className={styles.List} data-testid="List">
+    List Component
+  </div>
+);
+
+export default List;
